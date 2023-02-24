0f67d94eb6da0ba6d8e4e2fe4078b2d6c812de88
dungna <dungna@smartosc.com>
2023-02-24 21:48:31 +0700

    [day-1] setup folder project

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+const morgan = require('morgan')
+const helmet = require('helmet')
+const compression = require('compression')
+
+
+//--------- init middlewares
+app.use(morgan("dev")) // morgan("combined") -> nen su dung o mode PRODUCTION / morgan("common") thong bao thieu thong tin / morgan("short") / morgan("tiny")
+app.use(helmet()) // bao ve thong tin rieng tu
+app.use(compression()) //giam tai bang thong
+//--------- init database
+
+//--------- init router
+app.get('/', (req, res, next) => {
+    const strCompress = 'Hello Dev!!!'
+    return res.status(500).json({
+        message: "welcome dev!!!",
+        metadata: strCompress.repeat(10000)
+    })
+})
+
+//--------- handling errors
+
+module.exports = app
