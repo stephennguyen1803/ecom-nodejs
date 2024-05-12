161b6b2ac8138d5525a2d49d35a36242715dd677
Anh Dung Nguyen <anhdung.phc@gmail.com>
2024-05-12 22:06:10 +0700

    Remove Try Cache it will throw the error and it will be handle in route

--- src/app.js
+++ src/app.js
@@ -24,5 +24,19 @@ checkOverLoad()
 app.use('',require('./routers/index'))
 
 //--------- handling errors
+app.use((req, res, next) => {
+    const error = new Error('Not found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    const statusCode = error.status || 500
+    return res.status(statusCode).json({
+        status: 'error',
+        code: statusCode,
+        message: error.message || 'Internal Server Error'
+    })
+})
 
 module.exports = app
