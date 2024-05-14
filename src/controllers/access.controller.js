26bfb27a31bdf0558c71042f19f0c727f48a302c
Anh Dung Nguyen <anhdung.phc@gmail.com>
2024-05-14 23:08:01 +0700

    Using core handle success response

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,9 +1,16 @@
 'use strict'
 
 const AccessService = require("../services/access.service")
+const { OK, CREATED } = require("../core/success.response")
 
 class AccessController {
 
+    /**
+     * Create new Shop
+     * @param {Object} req 
+     * @param {Object} res 
+     * @param {Object} next 
+     */
     signUp = async(req, res, next) => {
         // try {
         console.log('[P]::signUp::', req.body)
@@ -11,9 +18,17 @@ class AccessController {
          * 200 OK
          * 201 CREATED
          */
-        return res.status(201).json(await AccessService.signUp(
-            req.body,
-        ))
+
+        new CREATED({
+            message: 'Registered OK!',
+            metaData: await AccessService.signUp(req.body),
+            options: {
+                limit: 10
+            }
+        }).send({res})
+        // return res.status(201).json(await AccessService.signUp(
+        //     req.body,
+        // ))
         // } catch(error) {
         //     next(error)
         // }
