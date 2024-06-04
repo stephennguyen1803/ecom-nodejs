4ce77a391a520ecedf09e1690ddd870e13979b1f
Anh Dung Nguyen <anhdung.phc@gmail.com>
2024-06-04 22:42:09 +0700

    section14 optimize fix some issue relate to product service

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -12,7 +12,7 @@ const productSchema = new Schema({
     product_description:{type:String},
     product_price:{type:Number, required:true},
     product_quantity:{type:Number, required:true},
-    product_type:{type:String, required:true, enum:['Electronisc','Clothing', 'Funiture', 'Books', 'Others']},
+    product_type:{type:String, required:true, enum:['Electronic','Clothing', 'Funiture', 'Books', 'Others']},
     product_category:{type:String},
     product_shop:{type:Schema.Types.ObjectId, ref:'Shop', required:true},
     product_attributes:{type: Schema.Types.Mixed, required:true}
